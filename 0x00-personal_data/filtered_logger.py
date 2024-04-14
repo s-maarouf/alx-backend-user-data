3cc1930aeb247dde87e6b983e93c0012972a5171
s-maarouf <maarouf.salahedd@gmail.com>
2024-04-14 00:46:43 +0000

    Fixed output requ

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -45,8 +45,8 @@ def get_logger() -> logging.Logger:
     """Returns a log object."""
     log = logging.getLogger("user_data")
     stream = logging.StreamHandler
-    log.setLevel(logging.INFO)
     stream.setFormatter(RedactingFormatter(PII_FIELDS))
+    log.setLevel(logging.INFO)
     log.propagate = False
     log.addHandler(stream)
     return log
