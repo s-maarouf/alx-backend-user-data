7b89d2a277b4b60b571926ec4b63207fe03c01ba
s-maarouf <maarouf.salahedd@gmail.com>
2024-04-18 23:21:55 +0100

    Completed task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,14 +4,26 @@
 
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
     """Authentification class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns False"""
-        return False
+        """Checks if path is not in the excluded paths"""
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns None"""
