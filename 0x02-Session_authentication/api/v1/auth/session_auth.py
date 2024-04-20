5db93ac5d038b9bfd119dbc9556fcce684b20b89
s-maarouf <maarouf.salahedd@gmail.com>
2024-04-20 15:23:12 +0100

    Completed task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,20 @@
 
 """Session autentification module"""
 
+from uuid import uuid4
+
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Session auth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session ID for a user"""
+        if type(user_id) is not str or user_id is None:
+            return None
+        else:
+            session = str(uuid4())
+            self.user_id_by_session_id[session] = user_id
+            return session
