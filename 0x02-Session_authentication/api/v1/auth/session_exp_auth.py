acbe27f92bfd6f1897cb4f8d35328fe5ac45e880
s-maarouf <maarouf.salahedd@gmail.com>
2024-04-20 22:35:18 +0100

    Updated implementation

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -32,14 +32,12 @@ class SessionExpAuth(SessionAuth):
     def user_id_for_session_id(self, session_id=None):
         """returns userid assosiated with sessionid"""
         session_dict = self.user_id_by_session_id[session_id]
-        if session_id is None:
-            return None
-        if not self.user_id_by_session_id[session_id]:
-            return None
-        if self.session_duration <= 0:
-            return session_dict['user_id']
-        if 'created_at' not in session_dict:
-            return None
+        if session_id in self.user_id_by_session_id:
+            session_dict = self.user_id_by_session_id[session_id]
+            if self.session_duration <= 0:
+                return session_dict['user_id']
+            if 'created_at' not in session_dict:
+                return None
         curr_time = datetime.datetime.now()
         span = datetime.timedelta(seconds=self.session_duration)
         exp_time = session_dict['created_at'] + span
