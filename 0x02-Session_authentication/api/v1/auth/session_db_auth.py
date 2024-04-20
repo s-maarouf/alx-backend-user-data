39709d9d564ae3da79dc81d1b7355302cf04c280
s-maarouf <maarouf.salahedd@gmail.com>
2024-04-21 00:03:58 +0100

    PEP8 fix

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -16,7 +16,7 @@ class SessionDBAuth(SessionExpAuth):
         """Creates and stores a session id for the user.
         """
         session_id = super().create_session(user_id)
-        if type(session_id) == str:
+        if type(session_id) is str:
             kwargs = {
                 'user_id': user_id,
                 'session_id': session_id,
